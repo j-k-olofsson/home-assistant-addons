59d9f5e74f2b132a61094ff4d805aa1364e5fe06
Keno <j.k.olofsson@gmail.com>
2025-07-01 16:03:58 +0200

    Delete chromadb/services.d/chromadb/run.sh

--- chromadb/services.d/chromadb/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-exec uvicorn chromadb.app:app \
-  --workers 1 \
-  --host 0.0.0.0 \
-  --port 8000 \
-  --proxy-headers \
-  --log-config chromadb/log_config.yml \
-  --timeout-keep-alive 30
